ece272b72b912d02b3b645917308d3058dda9de7
Viesoft <viesoft.tech@gmail.com>
2022-02-26 19:44:27 +0530

    Updated 2.0.0

--- hellocharts-samples/src/main/java/lecho/lib/hellocharts/samples/ViewPagerChartsActivity.java
+++ hellocharts-samples/src/main/java/lecho/lib/hellocharts/samples/ViewPagerChartsActivity.java
@@ -1,13 +1,15 @@
 package lecho.lib.hellocharts.samples;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,12 +39,12 @@ import lecho.lib.hellocharts.view.LineChartView;
 import lecho.lib.hellocharts.view.PieChartView;
 import lecho.lib.hellocharts.view.PreviewLineChartView;
 
-public class ViewPagerChartsActivity extends ActionBarActivity implements ActionBar.TabListener {
+public class ViewPagerChartsActivity extends AppCompatActivity implements ActionBar.TabListener {
 
     /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide fragments for each of the sections. We use a
+     * The {@link PagerAdapter} that will provide fragments for each of the sections. We use a
      * {@link FragmentPagerAdapter} derivative, which will keep every loaded fragment in memory. If this becomes too
-     * memory intensive, it may be best to switch to a {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     * memory intensive, it may be best to switch to a {@link FragmentStatePagerAdapter}.
      */
     SectionsPagerAdapter mSectionsPagerAdapter;
 
